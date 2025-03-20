b0cfe96b8fdc89e91035945299d516946a4ca5d6
marquessam <samuelnmarques@gmail.com>
2025-03-20 08:30:21 +0800

    Create backgrounds.js

---
+++ src/data/characterRandomizer/backgrounds.js
@@ -0,0 +1,28 @@
+// List of all available backgrounds
+export const backgrounds = [
+  'Worked as a chef on a river gambling boat',
+  'Served as a soldier during the War of the 4th Age',
+  'Was a librarian\'s assistant in Fairhaven',
+  'Grew up as a street urchin in the slums of Blackwater',
+  'Trained as an apprentice to a master blacksmith',
+  'Studied at the arcane academy of Silverhold',
+  'Was a smuggler operating along the Serpent\'s Coast',
+  'Served as an acolyte in the Temple of Eternal Light',
+  'Was a grave digger with a reputation for talking to the dead',
+  'Worked as a silk farmer raising giant spiders',
+  'Served as a professional curse breaker with a mixed reputation',
+  'Was a charcoal burner living in the deep woods',
+  // You can expand this list as much as you want
+  'Captained a merchant vessel with a secret smuggling compartment',
+  'Worked as a royal court jester with access to state secrets',
+  'Was an apothecary specializing in rare and dangerous herbs',
+  'Served as a bounty hunter tracking down magical fugitives',
+  'Was a pit fighter famous for never losing a match',
+  'Worked as a cartographer mapping uncharted territories',
+  'Maintained ancient mechanisms in a dwarven underground city',
+  'Ran a traveling carnival that specialized in exotic creatures',
+  'Was a wilderness guide for wealthy nobles on hunting expeditions',
+  'Served as a diplomat between warring factions',
+  'Was a vintner who cultivated magical grapes',
+  'Worked as a courier delivering sensitive messages across dangerous lands'
+];
