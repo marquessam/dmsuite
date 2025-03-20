8d322261f4fe9c3615b85a7924adcd068a2992db
marquessam <samuelnmarques@gmail.com>
2025-03-20 08:59:16 +0800

    Create characterRandomizerData.js

---
+++ src/data/characterRandomizerData.js
@@ -0,0 +1,8 @@
+// This file combines all character randomizer data into a single export
+export { races, raceDescriptions } from './characterRandomizer/races.js';
+export { classes, classDescriptions } from './characterRandomizer/classes.js';
+export { backgrounds } from './characterRandomizer/backgrounds.js';
+export { features } from './characterRandomizer/features.js';
+export { appearances } from './characterRandomizer/appearances.js';
+export { deities } from './characterRandomizer/deities.js';
+export { namePrefixes, nameSuffixes, lastNames } from './characterRandomizer/nameComponents.js';
