87d9c4249fb4f8e85d86754e6e2955d69a6361e8
marquessam <samuelnmarques@gmail.com>
2025-03-20 08:31:42 +0800

    Create nameComponents.js

---
+++ src/data/characterRandomizer/nameComponents.js
@@ -0,0 +1,17 @@
+// Name generation components
+export const namePrefixes = ['Ar', 'Bel', 'Cal', 'Dor', 'El', 'Fir', 'Gor', 'Hal', 'Il', 'Jor', 'Kal', 'Lor', 'Mor', 'Nor', 'Or',
+  // You can expand this list as much as you want
+  'Zan', 'Vex', 'Thar', 'Syl', 'Ryn', 'Qar', 'Pyr', 'Mar', 'Kil', 'Jax', 'Hel', 'Grim', 'Far', 'Drak', 'Crim'
+];
+
+export const nameSuffixes = ['an', 'en', 'ian', 'ion', 'ir', 'or', 'us', 'ius', 'ath', 'eth', 'ith', 'oth', 'uth', 'ak', 'ek',
+  // You can expand this list as much as you want
+  'ax', 'ix', 'ox', 'ux', 'ain', 'oon', 'ard', 'orn', 'iel', 'wyn', 'wyth', 'zar', 'zer', 'gar', 'mar'
+];
+
+export const lastNames = ['Blackwood', 'Silverleaf', 'Ironforge', 'Stormwind', 'Nightshade', 'Firehand', 'Dawnbringer', 'Moonshadow', 
+  'Thunderheart', 'Stonehelm', 'Oakenshield', 'Windrider', 'Starseeker', 'Flamecaller', 'Frostbeard',
+  // You can expand this list as much as you want
+  'Shadowweaver', 'Lightbringer', 'Steelclaw', 'Grimtooth', 'Swiftarrow', 'Rocksplitter', 'Cloudchaser', 'Beastmaster',
+  'Truthseeker', 'Gloomhaven', 'Farseer', 'Bloodraven', 'Goldspinner', 'Iceheart', 'Thornwalker'
+];
