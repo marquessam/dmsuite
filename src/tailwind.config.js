c1edc8ec77bf2cbe67aa65116b3022826ceef7ef
marquessam <samuelnmarques@gmail.com>
2025-03-20 09:05:50 +0800

    Update tailwind.config.js

--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -2,9 +2,25 @@
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        stone: {
+          800: '#292524',
+          900: '#1c1917'
+        },
+        amber: {
+          100: '#fef3c7',
+          200: '#fde68a',
+          700: '#b45309',
+          800: '#92400e',
+          900: '#78350f',
+          950: '#451a03'
+        }
+      }
+    },
   },
   plugins: [],
 }
