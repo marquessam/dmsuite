b5ac3e76bddb20456f9984bc7c51bb5639dc74cd
marquessam <samuelnmarques@gmail.com>
2025-03-21 08:19:07 +0800

    Update index.js

--- src/data/dmscreen/index.js
+++ src/data/dmscreen/index.js
@@ -3,31 +3,86 @@
 
 import { conditionsData } from './conditions';
 import { rulesData } from './rules';
-import { combatData } from './combat';
+import { combatData, combatFlow, coverRules } from './combat';
 import { itemsData } from './items';
-import { magicItemsData } from './magicItems';
-import { weaponsData } from './weapons';
-import { armorData } from './armor';
+import { magicItemsData, magicItemTables } from './magicItems';
+import { weaponsData, weaponProperties } from './weapons';
+import { armorData, armorRules } from './armor';
 import { spellsData } from './spells';
-import { classFeatures } from './classFeatures';
+import { classFeatures, classProficiencies } from './classFeatures';
 import { archetypes } from './archetypes';
-import { encounterData } from './encounters';
-import { npcData } from './npcs';
-import { diceTypes, dicePresets } from './dice';
+import { encounterData, monstersByEnvironment, encounterBalancing } from './encounters';
+import { npcData, nameComponents, npcStatBlocks } from './npcs';
+import { diceTypes, dicePresets, diceUtils } from './dice';
+import { monsters, monsterTypes, monsterEnvironments, legendaryMonsters, monsterTraits, monsterActionsByType } from './monsters';
+import { 
+  locationTypes, 
+  castleGenerator, 
+  tavernGenerator, 
+  dungeonGenerator, 
+  settlementGenerator, 
+  wildernessLocations, 
+  trapGenerator, 
+  roomFeatures 
+} from './locations';
 
 export {
+  // Conditions
   conditionsData,
+  
+  // Rules
   rulesData,
+  
+  // Combat
   combatData,
+  combatFlow,
+  coverRules,
+  
+  // Items
   itemsData,
   magicItemsData,
+  magicItemTables,
+  
+  // Weapons & Armor
   weaponsData,
+  weaponProperties,
   armorData,
+  armorRules,
+  
+  // Character Classes
   spellsData,
   classFeatures,
+  classProficiencies,
   archetypes,
+  
+  // Encounters & NPCs
   encounterData,
+  monstersByEnvironment,
+  encounterBalancing,
   npcData,
+  nameComponents,
+  npcStatBlocks,
+  
+  // Dice
   diceTypes,
-  dicePresets
+  dicePresets,
+  diceUtils,
+  
+  // Monsters
+  monsters,
+  monsterTypes,
+  monsterEnvironments,
+  legendaryMonsters,
+  monsterTraits,
+  monsterActionsByType,
+  
+  // Locations
+  locationTypes,
+  castleGenerator,
+  tavernGenerator,
+  dungeonGenerator,
+  settlementGenerator,
+  wildernessLocations,
+  trapGenerator,
+  roomFeatures
 };
