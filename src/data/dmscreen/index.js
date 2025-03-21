af7f5de7a93455b4edbce0f0e6bf4ba225430bef
marquessam <samuelnmarques@gmail.com>
2025-03-21 08:11:34 +0800

    Create index.js

---
+++ src/data/dmscreen/index.js
@@ -0,0 +1,33 @@
+// src/data/dmscreen/index.js
+// This file exports all the DM Screen data from individual files
+
+import { conditionsData } from './conditions';
+import { rulesData } from './rules';
+import { combatData } from './combat';
+import { itemsData } from './items';
+import { magicItemsData } from './magicItems';
+import { weaponsData } from './weapons';
+import { armorData } from './armor';
+import { spellsData } from './spells';
+import { classFeatures } from './classFeatures';
+import { archetypes } from './archetypes';
+import { encounterData } from './encounters';
+import { npcData } from './npcs';
+import { diceTypes, dicePresets } from './dice';
+
+export {
+  conditionsData,
+  rulesData,
+  combatData,
+  itemsData,
+  magicItemsData,
+  weaponsData,
+  armorData,
+  spellsData,
+  classFeatures,
+  archetypes,
+  encounterData,
+  npcData,
+  diceTypes,
+  dicePresets
+};
