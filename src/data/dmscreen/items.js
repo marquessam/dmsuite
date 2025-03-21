c8914caa016c08c89598b16a3a99a426b62895bf
marquessam <samuelnmarques@gmail.com>
2025-03-21 08:11:53 +0800

    Create items.js

---
+++ src/data/dmscreen/items.js
@@ -0,0 +1,26 @@
+// src/data/dmscreen/items.js
+// Regular adventuring gear and equipment
+
+export const itemsData = [
+  {
+    id: "adventuring-gear",
+    name: "Adventuring Gear",
+    category: "gear",
+    type: "Adventuring Gear",
+    items: [
+      { name: "Backpack", cost: "2 gp", weight: "5 lb.", description: "Holds up to 1 cubic foot/30 pounds of gear" },
+      // Additional items will be added here
+    ]
+  },
+  {
+    id: "tools",
+    name: "Tools",
+    category: "gear",
+    type: "Tools",
+    items: [
+      { name: "Thieves' tools", cost: "25 gp", weight: "1 lb." },
+      // Additional tools will be added here
+    ]
+  },
+  // Additional item categories will be added here
+];
