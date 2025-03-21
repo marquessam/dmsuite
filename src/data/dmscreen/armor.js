dcddf696e7b4966144a8675c38971659b23aa3d6
marquessam <samuelnmarques@gmail.com>
2025-03-21 08:39:39 +0800

    Update armor.js

--- src/data/dmscreen/armor.js
+++ src/data/dmscreen/armor.js
@@ -3,34 +3,39 @@
 
 export const armorData = [
   {
-    id: "light-armor",
-    name: "Light Armor",
-    category: "armor",
-    type: "Light Armor",
-    items: [
-      { name: "Padded", cost: "5 gp", ac: "11 + Dex modifier", strength: "", stealth: "Disadvantage", weight: "8 lb." },
-      { name: "Leather", cost: "10 gp", ac: "11 + Dex modifier", strength: "", stealth: "", weight: "10 lb." },
-      // Additional light armor will be added here
+     "id": "light-armor",
+    "name": "Light Armor",
+    "category": "armor",
+    "type": "Light Armor",
+    "items": [
+      { "name": "Padded", "cost": "5 gp", "ac": "11 + Dex modifier", "strength": "", "stealth": "Disadvantage", "weight": "8 lb." },
+      { "name": "Leather", "cost": "10 gp", "ac": "11 + Dex modifier", "strength": "", "stealth": "", "weight": "10 lb." },
+      { "name": "Studded Leather", "cost": "45 gp", "ac": "12 + Dex modifier", "strength": "", "stealth": "", "weight": "13 lb." }
     ]
   },
   {
-    id: "medium-armor",
-    name: "Medium Armor",
-    category: "armor",
-    type: "Medium Armor",
-    items: [
-      { name: "Hide", cost: "10 gp", ac: "12 + Dex modifier (max 2)", strength: "", stealth: "", weight: "12 lb." },
-      // Additional medium armor will be added here
+    "id": "medium-armor",
+    "name": "Medium Armor",
+    "category": "armor",
+    "type": "Medium Armor",
+    "items": [
+      { "name": "Hide", "cost": "10 gp", "ac": "12 + Dex modifier (max 2)", "strength": "", "stealth": "", "weight": "12 lb." },
+      { "name": "Chain Shirt", "cost": "50 gp", "ac": "13 + Dex modifier (max 2)", "strength": "", "stealth": "", "weight": "20 lb." },
+      { "name": "Scale Mail", "cost": "50 gp", "ac": "14 + Dex modifier (max 2)", "strength": "", "stealth": "Disadvantage", "weight": "45 lb." },
+      { "name": "Breastplate", "cost": "400 gp", "ac": "14 + Dex modifier (max 2)", "strength": "", "stealth": "", "weight": "20 lb." },
+      { "name": "Half Plate", "cost": "750 gp", "ac": "15 + Dex modifier (max 2)", "strength": "", "stealth": "Disadvantage", "weight": "40 lb." }
     ]
   },
   {
-    id: "heavy-armor",
-    name: "Heavy Armor",
-    category: "armor",
-    type: "Heavy Armor",
-    items: [
-      { name: "Ring mail", cost: "30 gp", ac: "14", strength: "", stealth: "Disadvantage", weight: "40 lb." },
-      // Additional heavy armor will be added here
+    "id": "heavy-armor",
+    "name": "Heavy Armor",
+    "category": "armor",
+    "type": "Heavy Armor",
+    "items": [
+      { "name": "Ring Mail", "cost": "30 gp", "ac": "14", "strength": "", "stealth": "Disadvantage", "weight": "40 lb." },
+      { "name": "Chain Mail", "cost": "75 gp", "ac": "16", "strength": "Str 13", "stealth": "Disadvantage", "weight": "55 lb." },
+      { "name": "Splint", "cost": "200 gp", "ac": "17", "strength": "Str 15", "stealth": "Disadvantage", "weight": "60 lb." },
+      { "name": "Plate", "cost": "1500 gp", "ac": "18", "strength": "Str 15", "stealth": "Disadvantage", "weight": "65 lb." }
     ]
   },
   {
